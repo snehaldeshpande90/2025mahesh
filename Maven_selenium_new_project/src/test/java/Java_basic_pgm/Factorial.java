8a27e7998d5f2655f866476a450a9dea370acfb0
Admin <Admin@DESKTOP-I87U9OT>
2026-05-08 13:41:08 +0530

    Updated factorial input to 6 and practicing git push

---
+++ Maven_selenium_new_project/src/test/java/Java_basic_pgm/Factorial.java
@@ -0,0 +1,17 @@
+package Java_basic_pgm;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x=6;
+		int fact=1;
+		for(int i=1;i<=5;i++)
+		{
+			fact=fact*x;
+		}
+		System.out.println(fact);
+	}
+
+}
