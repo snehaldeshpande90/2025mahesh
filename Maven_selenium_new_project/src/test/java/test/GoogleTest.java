e2139e066fbf4e5b06bce90408680a4fc8a6159f
Admin <Admin@DESKTOP-I87U9OT>
2026-05-11 22:44:02 +0530

    Added headless mode for CI CD

--- Maven_selenium_new_project/src/test/java/test/GoogleTest.java
+++ Maven_selenium_new_project/src/test/java/test/GoogleTest.java
@@ -1,14 +1,22 @@
 package test;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions; // ही ओळ जोडा
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class GoogleTest {
     @Test
     public void testGoogleTitle() {
-        // WebDriverManager किंवा Selenium 4 आपोआप ड्रायव्हर मॅनेज करेल
-        WebDriver driver = new ChromeDriver();
+        // हेडलेस मोड सेट करण्यासाठी खालील ३ ओळी जोडा
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless"); 
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        WebDriver driver = new ChromeDriver(options); // 'options' इथे पास करा
+        
         driver.get("https://www.google.com");
         String title = driver.getTitle();
         Assert.assertEquals(title, "Google");
