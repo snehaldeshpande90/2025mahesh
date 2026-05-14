08e6deaf68c7db199a2e5aac32ee40b1f7cfcbdc
Admin <Admin@DESKTOP-I87U9OT>
2026-05-14 10:53:41 +0530

    fix: add explicit wait to handle OrangeHRM login loading issue

--- Maven_selenium_new_project/src/main/java/com/pages/LoginPage.java
+++ Maven_selenium_new_project/src/main/java/com/pages/LoginPage.java
@@ -1,10 +1,14 @@
 package com.pages;
 
+import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage {
     WebDriver driver;
+    WebDriverWait wait;
 
     // लोकेटर्स
     By usernameField = By.name("username");
@@ -13,11 +17,13 @@ public class LoginPage {
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
+        // १० सेकंदांचा वेट (Wait) सेट करणे
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
-    // ॲक्शन्स
     public void login(String user, String pass) {
-        driver.findElement(usernameField).sendKeys(user);
+        // 'username' फील्ड दिसेपर्यंत थांबणे
+        wait.until(ExpectedConditions.visibilityOfElementLocated(usernameField)).sendKeys(user);
         driver.findElement(passwordField).sendKeys(pass);
         driver.findElement(loginButton).click();
     }
