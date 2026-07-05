337e94fd314760e3c01f041e1a8aac4c3d32b20b
Admin <Admin@DESKTOP-I87U9OT>
2026-07-05 14:14:23 +0530

    first login sucess

--- Maven_selenium_new_project/src/test/java/com/base/BaseTest.java
+++ Maven_selenium_new_project/src/test/java/com/base/BaseTest.java
@@ -20,7 +20,7 @@ public class BaseTest {
     @AfterMethod
     public void tearDown() {
         if (driver != null) {
-            driver.quit();
+            //driver.quit();
         }
     }
 }
