f9cbbc988a2138207f35bcea829e67c3e43bf58c
Admin <Admin@DESKTOP-I87U9OT>
2026-07-05 14:32:46 +0530

    adding depeddancy

--- Maven_selenium_new_project/src/test/java/com/base/BaseTest.java
+++ Maven_selenium_new_project/src/test/java/com/base/BaseTest.java
@@ -5,12 +5,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import java.time.Duration;
+import io.github.bonigarcia.wdm.WebDriverManager; // WebDriverManager import करा
 
 public class BaseTest {
     public WebDriver driver;
 
     @BeforeMethod
     public void setup() {
+        // WebDriverManager वापरून ब्राउझर ड्रायव्हर ऑटोमॅटिक मॅनेज करा
+        WebDriverManager.chromedriver().setup();
+        
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
@@ -19,8 +23,9 @@ public class BaseTest {
 
     @AfterMethod
     public void tearDown() {
+        // ब्राउझर बंद करणे गरजेचे आहे, जेणेकरून बॅकग्राउंडमध्ये मेमरी वापरली जाणार नाही
         if (driver != null) {
-            //driver.quit();
+            driver.quit();
         }
     }
 }
